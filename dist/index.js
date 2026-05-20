dfaea616b9deb5dd973ba477767bf476372e6718
kiran kumar <kiranatmakuri518@gmail.com>
2026-05-20 22:24:05 +0530

    fix: add deploy compatibility entrypoint

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Compatibility boot file for DigitalOcean's current run_command.
+// The real compiled server now lives in jago_app-main/app/dist/index.js.
+require("../app/dist/index.js");
